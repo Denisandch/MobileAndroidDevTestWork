e19745bd15325dbc7405f27578cfd8ee6319c646
Denis Samolovov <denissamolovov@gmail.com>
2023-05-01 14:22:16 +0300

    get_data logic moved to repository deleted optional type converter: was server.deal -> deal -> dealDB -> deal now server.dead -> dealDB -> deal

--- app/src/main/java/com/ntpro/mobileandroiddevtestwork/DealPagingSource.kt
+++ app/src/main/java/com/ntpro/mobileandroiddevtestwork/DealPagingSource.kt
@@ -2,13 +2,13 @@ package com.ntpro.mobileandroiddevtestwork
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import com.ntpro.mobileandroiddevtestwork.room.DealDB
 import com.ntpro.mobileandroiddevtestwork.room.DealDao
 
+typealias DealsLoader = suspend (page: Int, pageSize: Int) -> List<Deal>
+
 class DealPagingSource(
-    private val dealDao: DealDao,
-    private val column: Column,
-    private val direction: Boolean,
-    private val maxID: Long
+    private val dealsLoader: DealsLoader,
 ) : PagingSource<Int, Deal>() {
     override fun getRefreshKey(state: PagingState<Int, Deal>): Int? {
         val anchorPosition = state.anchorPosition ?: return null
@@ -21,44 +21,7 @@ class DealPagingSource(
         val pageSize: Int = params.loadSize.coerceAtMost(50)
 
         return try {
-            val response = when (column) {
-                Column.INSTRUMENT_NAME -> {
-                    if (direction) {
-                        dealDao.getAllDealsByInstrumentNameAsc(pageSize, page * pageSize, maxID)
-                    } else {
-                        dealDao.getAllDealsByInstrumentNameDesc(pageSize, page * pageSize, maxID)
-                    }
-                }
-                Column.TIME_STAMP -> {
-                    if (direction) {
-                        dealDao.getAllDealsByDateAsc(pageSize, page * pageSize, maxID)
-                    } else {
-                        dealDao.getAllDealsByDateDesc(pageSize, page * pageSize, maxID)
-                    }
-                }
-                Column.PRICE -> {
-                    if (direction) {
-                        dealDao.getAllDealsByPriceAsc(pageSize, page * pageSize, maxID)
-                    } else {
-                        dealDao.getAllDealsByPriceDesc(pageSize, page * pageSize, maxID)
-                    }
-                }
-                Column.AMOUNT -> {
-                    if (direction) {
-                        dealDao.getAllDealsByAmountAsc(pageSize, page * pageSize, maxID)
-                    } else {
-                        dealDao.getAllDealsByAmountDesc(pageSize, page * pageSize, maxID)
-                    }
-                }
-                Column.SIDE -> {
-                    if (direction) {
-                        dealDao.getAllDealsBySideAsc(pageSize, page * pageSize, maxID)
-                    } else {
-                        dealDao.getAllDealsBySideDesc(pageSize, page * pageSize, maxID)
-                    }
-                }
-            }.map { it.toDeal() }
-
+            val response = dealsLoader.invoke(page, pageSize)
 
             val nextKey = if (response.size < pageSize) null else page + 1
             val prevKey = if (page == 0) null else page - 1
--- app/src/main/java/com/ntpro/mobileandroiddevtestwork/repository/DealRepositoryImplementation.kt
+++ app/src/main/java/com/ntpro/mobileandroiddevtestwork/repository/DealRepositoryImplementation.kt
@@ -9,6 +9,7 @@ import com.ntpro.mobileandroiddevtestwork.room.DealDao
 import com.ntpro.mobileandroiddevtestwork.room.DealDataBase
 import kotlinx.coroutines.flow.Flow
 
+
 class DealRepositoryImplementation(context: Context) : DealRepository {
 
     init {
@@ -18,18 +19,68 @@ class DealRepositoryImplementation(context: Context) : DealRepository {
     private val dealDao: DealDao = DealDataBase.getDatabase(context).dealDao()
     override fun getDeals(filter: Column, isAsc: Boolean, maxID: Long): Flow<PagingData<Deal>> {
 
+        val dealsLoader: DealsLoader = { page, pageSize ->
+            selectQuery(page, pageSize, filter, isAsc, maxID)
+        }
+
         return Pager<Int, Deal>(
-            PagingConfig (
+            PagingConfig(
                 pageSize = 50,
                 prefetchDistance = 100,
                 initialLoadSize = 250
             ),
         ) {
-            DealPagingSource(dealDao, column = filter, direction = isAsc, maxID = maxID)
+            DealPagingSource(dealsLoader)
         }.flow
     }
 
     override suspend fun addNewDeals(deals: List<Server.Deal>) {
         dealDao.addListDeal(deals.map { it.toDealDB() })
+    }
+    private suspend fun selectQuery(
+        page: Int,
+        pageSize: Int,
+        column: Column,
+        direction: Boolean,
+        maxID: Long
+    ): List<Deal> {
+
+        return when (column) {
+            Column.INSTRUMENT_NAME -> {
+                if (direction) {
+                    dealDao.getAllDealsByInstrumentNameAsc(pageSize, page * pageSize, maxID)
+                } else {
+                    dealDao.getAllDealsByInstrumentNameDesc(pageSize, page * pageSize, maxID)
+                }
+            }
+            Column.TIME_STAMP -> {
+                if (direction) {
+                    dealDao.getAllDealsByDateAsc(pageSize, page * pageSize, maxID)
+                } else {
+                    dealDao.getAllDealsByDateDesc(pageSize, page * pageSize, maxID)
+                }
+            }
+            Column.PRICE -> {
+                if (direction) {
+                    dealDao.getAllDealsByPriceAsc(pageSize, page * pageSize, maxID)
+                } else {
+                    dealDao.getAllDealsByPriceDesc(pageSize, page * pageSize, maxID)
+                }
+            }
+            Column.AMOUNT -> {
+                if (direction) {
+                    dealDao.getAllDealsByAmountAsc(pageSize, page * pageSize, maxID)
+                } else {
+                    dealDao.getAllDealsByAmountDesc(pageSize, page * pageSize, maxID)
+                }
+            }
+            Column.SIDE -> {
+                if (direction) {
+                    dealDao.getAllDealsBySideAsc(pageSize, page * pageSize, maxID)
+                } else {
+                    dealDao.getAllDealsBySideDesc(pageSize, page * pageSize, maxID)
+                }
+            }
+        }.map { it.toDeal() }
     }
 }
